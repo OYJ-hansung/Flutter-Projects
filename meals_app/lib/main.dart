2b282bd5726abc6d7a76a32e322845ad3e9f2115
OYJ-hansung <dhdudwn95@hansung.ac.kr>
2022-03-23 12:37:36 +0900

    updates

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import './categories_screen.dart';
+import './screens/categories_screen.dart';
+import './screens/category_meals_screen.dart';
+import './screens/meal_detail_screen.dart';
+// import './screens/tabs_screen.dart';
+import './screens/tabs_screen_below.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,7 +27,23 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: CategoriesScreen(),
+      // home: CategoriesScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => TabsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
+      // onGenerateRoute: (settings) {
+      //   print(settings.arguments);
+      //   return MaterialPageRoute(
+      //     builder: (ctx) => CategoriesScreen(),
+      //   );
+      // },
+
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
     );
   }
 }
